e46272549d3fba4c49a9188f3299fc68a5a8cd1e
Jonathan C. Calixto <jonathanccalixto@gmail.com>
2020-11-01 19:56:50 -0300

    Routes implemented with react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import ChooseGame from 'pages/ChooseGame';
 
 const Routes: React.FC = () => {
-  return <ChooseGame />;
+  return (
+    <Switch>
+      <Route path="/" component={ChooseGame} />
+
+      <Redirect to="/" />
+    </Switch>
+  );
 };
 
 export default Routes;
